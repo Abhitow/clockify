9d57c26e4eaac775972ac8c593c165f9006f83d6
Abhitow <abhilash.rpee18@gmail.com>
2022-12-26 21:32:55 +0530

    project and report work

--- src/pages/Reports.js
+++ src/pages/Reports.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import React , {useState} from 'react'
+import { Button, Card, Row } from 'antd'
+import axios from 'axios'
 
 const Reports = () => {
+    const [post , setPost] = useState([]);
+    const handleReport = () =>{
+        console.log("<---------report handled");
+        axios.get("http://demo.emeetify.com:8080/daytodaytask/admin/getToatalCountProjects")
+        .then(response => {console.log(response)})
+        .catch(e => {console.log("e" , e)});
+    }
     return(
         <div>
             <h1>Welcome to Reports Page</h1>
+            <Row>
+                {post.map( (data) => {
+                    return (
+                        <div>
+                            <Card>
+                                
+                            </Card>
+                        </div>
+                    )
+                })}
+            </Row>
         </div>
     )
 }
